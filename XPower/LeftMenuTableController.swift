c766c983426a8690afa1e3e02842a37561b3c3b7
tqvcd <tqvcd2014@gmail.com>
2016-10-11 23:08:41 -0400

    Update LeftMenuTableController.swift

--- XPower/LeftMenuTableController.swift
+++ XPower/LeftMenuTableController.swift
@@ -12,24 +12,15 @@ import Parse
 class LeftMenuTableController: UITableViewController {
     
     let currUser = PFUser.currentUser()
-    
-    
+
     var leftSideMenuNames = ["Home", "Inventory", "Calendar", "Service", "Garage", "Settings", "About", "Logout"]
     
     var leftSideGuestMenuNames = ["Home", "Inventory", "Calendar", "Service", "Garage", "About", "Go to Login"]
 
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.separatorStyle = UITableViewCellSeparatorStyle.None
     
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-        
         if PFUser.currentUser() == nil || currUser!.username == "guest"{
         
             leftSideMenuNames = leftSideGuestMenuNames
@@ -86,16 +77,10 @@ class LeftMenuTableController: UITableViewController {
     func openController(controllerName:String) {
 
         let viewController = storyboard?.instantiateViewControllerWithIdentifier(controllerName)
-//        let mainNavigationController = storyboard?.instantiateViewControllerWithIdentifier("mainController") as! UINavigationController
         self.navigationController!.pushViewController(viewController!, animated: true)
         
     }
-    
-    
-    
-    
-    
-    
+     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
@@ -131,109 +116,7 @@ class LeftMenuTableController: UITableViewController {
             PFUser.logOut()
             self.openController("Login")
         }
-        
-        
-//        [tableView deselectRowAtIndexPath:indexPath animated:YES];
-//        
-//        switch (indexPath.row) {
-//        case 0:
-//            [self openController:_controllerNames[0]];
-//            break;
-//            
-//        case 1:
-//            [self openController:_controllerNames[1]];
-//            break;
-//            
-//        case 2:
-//            [self openController:_controllerNames[2]];
-//            break;
-//            
-//        case 3:
-//            [self openController:_controllerNames[3]];
-//            break;
-//            
-//        case 4:
-//            [self openController:_controllerNames[4]];
-//            break;
-//            
-//        case 5:
-//            [self openController:_controllerNames[5]];
-//            break;
-//            
-//        case 6:
-//            [self openControllerFromAnotherStoryBoard:_controllerNames[6]];
-//            break;
-//        case 7:
-//            [PFUser logOut];
-//            [[GIDSignIn sharedInstance] signOut];
-//            [[GIDSignIn sharedInstance] disconnect];
-//            [self openController:@"Login"];
-//            break;
-//        default:
-//            break;
-//        }
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+ 
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
