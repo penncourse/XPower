cc949d5a322b07193c874a2017dc9ac953ec5d9c
tqvcd <tqvcd2014@gmail.com>
2016-10-11 23:01:42 -0400

    Update RightMenuTableController.swift

--- XPower/RightMenuTableController.swift
+++ XPower/RightMenuTableController.swift
@@ -15,14 +15,7 @@ class RightMenuTableController: UITableViewController, UIAlertViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None;
-
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        self.tableView.separatorStyle = UITableViewCellSeparatorStyle.None
         
         let img = UIImage(named: "rightmenubackground")
         
@@ -60,7 +53,6 @@ class RightMenuTableController: UITableViewController, UIAlertViewDelegate {
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        
         let cell = tableView.dequeueReusableCellWithIdentifier("rightmenucell", forIndexPath: indexPath)
 
         // Configure the cell...
@@ -70,11 +62,8 @@ class RightMenuTableController: UITableViewController, UIAlertViewDelegate {
         
         cell.textLabel?.textColor = UIColor.blackColor()
         
-       
-        
         cell.textLabel?.font = UIFont(name: "HelveticaNeue", size: 24)
-        
-        
+           
         cell.backgroundColor = UIColor.clearColor()
         
         return cell
@@ -105,10 +94,7 @@ class RightMenuTableController: UITableViewController, UIAlertViewDelegate {
     }
 
     
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        
-        
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {  
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         switch indexPath.row {
         case 0:
@@ -123,59 +109,10 @@ class RightMenuTableController: UITableViewController, UIAlertViewDelegate {
         case 3:
             openController("invitation")
         
-            
         default:
              break
         }
-        
-        
+     
     }
-    
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
